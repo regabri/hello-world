b15d71a4a9e3cb5be3ef747c3b83795bad6ba600
khalid oubelque <khalidoubelque@gmail.com>
2020-05-17 16:11:37 +0200

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -17,7 +17,7 @@ node{
       stage('Deploy'){
             sshPublisher(publishers: 
                          [sshPublisherDesc(configName: 'dockerHost', 
-                                           transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'apt-get update', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/home/dockeradmin/webapp/target', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '*.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+                                           transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'apt-get update', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '*.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
       }
       
  }
