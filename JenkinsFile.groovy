193f0c534da8c3647227b342afb8c956f72f26f1
khalid oubelque <khalidoubelque@gmail.com>
2020-05-17 15:29:04 +0200

    Add JenkinsFile

---
+++ JenkinsFile.groovy
@@ -0,0 +1,22 @@
+node{
+      def mvnHome = tool name: 'M2_HOME', type: 'maven' 
+      stage('Checkout'){
+         git 'https://github.com/khalidOubelque/hello-world.git'
+       
+      }  
+      stage('Build'){
+         //// Get maven home path and build
+        sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
+      }
+     stage ('Test-JUnit'){
+         sh "'${mvnHome}/bin/mvn' test surefire-report:report"
+      }  
+    
+      stage('Deploy') {     
+            sshagent(['Tomcat-jenkins']) {
+               sh 'scp -o StrictHostKeyChecking=no target/tomcatdeploymnetdemo.war jenkins@35.193.54.220:/opt/tomcat/webapps'
+              
+          }   
+     }
+      
+ }
