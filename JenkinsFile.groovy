90ebb5e2b245f44254751934e61272fd8962f079
khalid oubelque <khalidoubelque@gmail.com>
2020-05-22 17:08:21 +0200

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -15,7 +15,7 @@ node{
       }  
      stage('Deploy') { 
             echo "KOU Deploy"
-           sshagent(['Jenkins-docker']) {
+           sshagent(['Jenkins_to_Docker']) {
                sh 'scp StrictHostKeyChecking=no webapp/target/*.war dockeradmin@172.31.35.165:/home/dockeradmin/webapp/target'
               
           }
