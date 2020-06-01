f48b477a1ca5ad4051ea28bc1f73cab178c85abc
khalid oubelque <khalidoubelque@gmail.com>
2020-06-01 11:15:08 +0200

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -8,7 +8,7 @@ pipeline{
              stage('Checkout'){
                    steps{
                         echo "KOU Checkout"
-                        git 'https://github.com/khalidOubelque/hello-world.git'
+                       // git 'https://github.com/khalidOubelque/hello-world.git'
                    }               
              }
        
@@ -16,14 +16,14 @@ pipeline{
             stage('Build'){
                   steps{
                      echo "KOU Maven Build"
-                     sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
+                     //sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
                   }
             }
             
              stage ('Test-JUnit'){
                    steps{
                      echo "KOU UT"
-                     sh "'${mvnHome}/bin/mvn' test surefire-report:report"
+                     //sh "'${mvnHome}/bin/mvn' test surefire-report:report"
                    }
             }
             
@@ -31,10 +31,10 @@ pipeline{
                   steps{
                         echo "KOU Deploy"
                         withCredentials([
-                              usernamePassword(credentialsId:'Jenkins_to_Docker', usernameVariable: USER, passwordVariable: PWD)
+                              usernamePassword(credentialsId:'Jenkins_to_Docker', usernameVariable: user, passwordVariable: pass)
                         ]){
-                              //sshpass -p ${PWD} sh 'scp StrictHostKeyChecking=no webapp/target/*.war ${USER}@172.31.35.165:/home/dockeradmin/webapp/target'
-                              echo "KOU Test credentials username  = ${USER}  and password = ${PWD} "
+                              //sshpass -p ${pass} sh 'scp StrictHostKeyChecking=no webapp/target/*.war ${user}@172.31.35.165:/home/dockeradmin/webapp/target'
+                              echo "KOU Test credentials username  = ${user}  and password = ${pass} "
 
                         }
                   }
