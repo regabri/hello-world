1b4975d398f1e58bbdf50494973c193d7cfe2f5d
khalid oubelque <khalidoubelque@gmail.com>
2020-06-01 10:54:48 +0200

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,27 +1,47 @@
-node{
-      def mvnHome = tool name: 'M2_HOME', type: 'maven' 
-      stage('Checkout'){
-         echo "KOU Checkout"
-         git 'https://github.com/khalidOubelque/hello-world.git'
-       
-      }  
-      stage('Build'){
-         echo "KOU Maven Build"
-        sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
+pipeline{
+      agent any
+      enviroment {
+            mvnHome = 'M2_HOME'
       }
-     stage ('Test-JUnit'){
-          echo "KOU UT"
-         sh "'${mvnHome}/bin/mvn' test surefire-report:report"
-      }  
-     stage('Deploy') { 
-            echo "KOU Deploy"
-   
-          sshagent(credentials:['deployer-user']) {
-               //sh 'scp StrictHostKeyChecking=no webapp/target/*.war dockeradmin@172.31.35.165:/home/dockeradmin/webapp/target'
-                       echo "KOU work"
+      stages{
+             stage('Checkout'){
+                   steps{
+                        echo "KOU Checkout"
+                        git 'https://github.com/khalidOubelque/hello-world.git'
+                   }               
+             }
+       
+            
+            stage('Build'){
+                  steps{
+                     echo "KOU Maven Build"
+                     sh "${mvnHome}/bin/mvn clean package -Dmaven.test.skip=true"
+                  }
+            }
+            
+             stage ('Test-JUnit'){
+                   steps{
+                     echo "KOU UT"
+                     sh "'${mvnHome}/bin/mvn' test surefire-report:report"
+                   }
+            }
+            
+            stage('Deploy') { 
+                  steps{
+                        echo "KOU Deploy"
+                        Withcredentials([
+                              usernamePassword(credentials:'Jenkins_to_Docker', usernameVariable: USER, passwordVariable: PWD)
+                        ]){
+                              //sshpass -p ${PWD} sh 'scp StrictHostKeyChecking=no webapp/target/*.war ${USER}@172.31.35.165:/home/dockeradmin/webapp/target'
+                              echo "KOU Test credentials username  = ${USER}  and password = ${PWD} "
 
-          }
-           
-        }
-      
+                        }
+                  }
+            }
+            
+            
+      } 
+    
  }
+      
+
